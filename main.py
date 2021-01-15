a566237666cdec44d884bc40eec83d76fe7d6cf8
HarlockOfficial <harlockofficial.github@gmail.com>
2021-01-15 11:08:54 +0100

    Updated Main & minor changes in .gitignore

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from telegram import Update, bot
 
 from time import time
 
-
+# used to block spam
 users = dict()
-otp = "0000"
+
 
 def is_banned(user: str) -> bool:
     with open("ban.log", "r") as f:
@@ -18,59 +18,70 @@ def is_banned(user: str) -> bool:
     return False
 
 
-def start(update: Update, context: CallbackContext):
+def is_admin(user: str) -> bool:
+    with open("admin.log", "r") as f:
+        if user in f.readlines():
+            return True
+    return False
+
+
+def start(update: Update, unused: CallbackContext):
     if is_banned(str(update.message.chat.username)+","+str(update.message.chat.id)):
         return
-    with open("users.log", "r+") as f:
-        content = f.readlines()
+    with open("users.log", "r+") as users_log:
+        content = users_log.readlines()
         if not str(update.message.chat.username)+","+str(update.message.chat.id) in content:
-            f.write(str(update.message.chat.username)+","+str(update.message.chat.id))
+            users_log.write(str(update.message.chat.username)+","+str(update.message.chat.id))
+            users_log.flush()
     update.message.reply_text("Benvenuto al Webinar di Sicurezza Informatica.\n" +
                               "Il bot sarà attivo solo durante i webinar.\n" +
                               "Per scoprire tutte le funzionalità, digita il comando /help")
 
 
-def bot_help(update: Update, context: CallbackContext):
+def bot_help(update: Update, unused: CallbackContext):
     update.message.reply_text("hai chiesto aiuto")
 
 
-def ask(update: Update, context: CallbackContext):
+def ask(update: Update, unused: CallbackContext):
     if is_banned(str(update.message.chat.username)+","+str(update.message.chat.id)):
         return
     if update.message.chat.username in users.keys() and time() - users[update.message.chat.username] <= 30:
         update.message.reply_text("Errore, puoi fare una domanda ogni 30 secondi")
         return
     users[update.message.chat.username] = time()
-    # Domande durante i webinar limite antispam 1 domanda/min
     update.message.reply_text("hai chiesto una cosa, presto riceverai una risposta")
-    pass
+    print(update.message.chat.username, "ha chiesto:", update.message.text)
 
 
-def info(update: Update, context: CallbackContext):
+def info(update: Update, unused: CallbackContext):
     # gruppo informatica, contatti e link (cartella/materiale/...)
     update.message.reply_text("info e link magici")
     pass
 
 
-def beer(update: Update, context: CallbackContext):
+def beer(update: Update, unused: CallbackContext):
     # paypal link
     update.message.reply_text("Se ti dovessero piacere i webinar, sentiti libero di offrirci una birra!\n")
     pass
 
 
-def today(update: Update, context: CallbackContext):
+def today(update: Update, unused: CallbackContext):
     update.message.reply_text("ferb, so cosa faremo oggi")
     pass
 
 
-def login(update: Update, context: CallbackContext):
-    # TODO implement add otp and login admin
-    pass
-
-
-def ban(update: Update, context: CallbackContext):
-    # TODO add user to ban.log by username (search in users.log)
-    pass
+def ban(update: Update, unused: CallbackContext):
+    if not is_admin(str(update.message.chat.username)+","+str(update.message.chat.id)):
+        return
+    user_to_ban = update.message.text
+    with open("users.log", "r") as users_log:
+        users_list = users_log.readlines()
+        for i in range(len(users_list)):
+            if user_to_ban == users_list[i].split(",")[0]:
+                with open("ban.log", "a") as ban_log:
+                    ban_log.write(users_list[i])
+                    ban_log.flush()
+                break
 
 
 def main():
